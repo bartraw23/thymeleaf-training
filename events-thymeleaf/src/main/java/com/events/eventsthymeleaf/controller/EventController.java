64b0bcc074ad055fe5215751456416bb172e2da8
Bartek <bartraw23@gmail.com>
2020-07-10 16:23:29 +0200

    Add basics for project start

---
+++ events-thymeleaf/src/main/java/com/events/eventsthymeleaf/controller/EventController.java
@@ -0,0 +1,24 @@
+package com.events.eventsthymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+    @GetMapping
+    public String displayAllEvents(Model model) {
+        List<String> events = new ArrayList<>();
+        events.add("Code");
+        events.add("Code wars");
+        events.add("Code");
+        events.add("Code wars");
+        model.addAttribute("events", events);
+        return "events/index";
+    }
+}
