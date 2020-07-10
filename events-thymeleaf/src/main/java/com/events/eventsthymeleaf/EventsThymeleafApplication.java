e926d43175147525aae13c40eb00208ffcf65a2d
Bartek <bartraw23@gmail.com>
2020-07-10 15:57:33 +0200

    Add start spring structure

---
+++ events-thymeleaf/src/main/java/com/events/eventsthymeleaf/EventsThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.events.eventsthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsThymeleafApplication.class, args);
+	}
+
+}
---
+++ events-thymeleaf/src/test/java/com/events/eventsthymeleaf/EventsThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.events.eventsthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
